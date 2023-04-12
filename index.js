cb97e34ac9e1d0c81f3d8e36a99f80eec86c5041
jason synsmir <jason.synsmir001@gmail.com>
2023-04-12 09:51:04 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+//defining currentUser
+ const currentUser = 'Grace Hopper'
+//defining welcomeMessage
+const welcomeMessage = "Welcome to Flatbook, " + "Grace Hopper" + "!";
+//defining  the value of the 'excitedWelcomeMessage' variable
+const excitedWelcomeMessage = ('WELCOME TO FLATBOOK, GRACE HOPPER' + '!')
+//defining  the value of the 'shortGreeting' variable
+const shortGreeting = ("Welcome, " + 'G' + '!')
